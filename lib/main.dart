2e5b3edfd2f6b5fe01188bea5c3977f8f6574d24
josemlegal <josemlegal97@gmail.com>
2023-07-09 19:11:00 -0400

    Add dependencies and setup the dotenv snackbar ui and locator into the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:login_flutter/core/dependency_injection/locator.dart';
+import 'package:login_flutter/core/dependency_injection/setup_snackbar_ui.dart';
 import 'package:login_flutter/core/router/router.dart' as router;
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  runApp(
+    const ProviderScope(child: MainApp()),
+  );
+  await dotenv.load();
+  setupLocator();
+  setupSnackbarUi();
 }
 
 class MainApp extends StatelessWidget {
